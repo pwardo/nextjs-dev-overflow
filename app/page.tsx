b610286499c27cecf28a67a8efa8f4e3167b9ce7
Patrick Ward <patwardo@gmail.com>
2023-10-26 11:15:53 +0100

    Create basic routes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJS DevOverflow!!</h1>
-      <h2 className="h2-bold">NextJS DevOverflow!!</h2>
-      <h3 className="h3-bold">NextJS DevOverflow!!</h3>
-    </div>
-  );
-}
