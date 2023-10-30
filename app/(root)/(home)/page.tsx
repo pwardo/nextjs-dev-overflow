31d142c31e97c5c402b97829eb56ce4fb9d14dfa
Patrick Ward <patwardo@gmail.com>
2023-10-30 11:03:23 +0000

    Auth setup using Clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default Home;
