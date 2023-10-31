b8beaaa1543a46ec89e6e50378672e660bb333f2
Patrick Ward <patwardo@gmail.com>
2023-10-31 16:33:35 +0000

    Add UI for GlobalSearch

---
+++ app/components/shared/search/GlobalSearch.tsx
@@ -0,0 +1,26 @@
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
+
+const GlobalSearch = () => {
+  return (
+    <div className="relative w-full max-w-[600px] max-lg:hidden">
+      <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="Search"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+        <Input
+          placeholder="Search"
+          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+          type="text"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GlobalSearch;
