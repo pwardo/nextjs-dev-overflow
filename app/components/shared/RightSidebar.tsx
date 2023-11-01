6849c74033741b4db61f55888bf3e7642304aefc
Patrick Ward <patwardo@gmail.com>
2023-11-01 16:00:50 +0000

    Setup RightSidebar component

---
+++ app/components/shared/RightSidebar.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+
+const topQuestions = [
+  {
+    _id: 1,
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?",
+  },
+  {
+    _id: 2,
+    title:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat?",
+  },
+  {
+    _id: 3,
+    title:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur?",
+  },
+  {
+    _id: 4,
+    title:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum?",
+  },
+  {
+    _id: 5,
+    title: "Tellus in hac habitasse platea dictumst?",
+  },
+];
+
+const RightSidebar = () => {
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {topQuestions.map((question) => (
+            <Link
+              href={`/questions/${question._id}`}
+              key={question._id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4"></div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
